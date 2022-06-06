e29ac6471fa11d7be894ffc77225031169e64010
Burak Onur Gül <burakonurgl1@outlook.com>
2022-06-07 01:17:05 +0300

    Tüm Dosyalar Var

---
+++ final2Tekrar/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace final2Tekrar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOgrNo = new System.Windows.Forms.TextBox();
+            this.txtOgrAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtOgrTel = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnResimSec = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtOgrenciAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(282, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 414);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Öğrenci Numarası: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Öğrenci Adı Soyadı: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Öğrenci Telefon Numarası: ";
+            // 
+            // txtOgrNo
+            // 
+            this.txtOgrNo.Location = new System.Drawing.Point(154, 12);
+            this.txtOgrNo.Name = "txtOgrNo";
+            this.txtOgrNo.Size = new System.Drawing.Size(122, 20);
+            this.txtOgrNo.TabIndex = 2;
+            // 
+            // txtOgrAdSoyad
+            // 
+            this.txtOgrAdSoyad.Location = new System.Drawing.Point(154, 35);
+            this.txtOgrAdSoyad.Name = "txtOgrAdSoyad";
+            this.txtOgrAdSoyad.Size = new System.Drawing.Size(122, 20);
+            this.txtOgrAdSoyad.TabIndex = 2;
+            // 
+            // txtOgrTel
+            // 
+            this.txtOgrTel.Location = new System.Drawing.Point(154, 58);
+            this.txtOgrTel.Name = "txtOgrTel";
+            this.txtOgrTel.Size = new System.Drawing.Size(122, 20);
+            this.txtOgrTel.TabIndex = 2;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(64, 344);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(141, 32);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(64, 382);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(141, 32);
+            this.btnGuncelle.TabIndex = 3;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(64, 420);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(141, 32);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 197);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnResimSec
+            // 
+            this.btnResimSec.Location = new System.Drawing.Point(64, 294);
+            this.btnResimSec.Name = "btnResimSec";
+            this.btnResimSec.Size = new System.Drawing.Size(141, 32);
+            this.btnResimSec.TabIndex = 5;
+            this.btnResimSec.Text = "Resim Seç";
+            this.btnResimSec.UseVisualStyleBackColor = true;
+            this.btnResimSec.Click += new System.EventHandler(this.btnResimSec_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtOgrenciAra
+            // 
+            this.txtOgrenciAra.Location = new System.Drawing.Point(282, 12);
+            this.txtOgrenciAra.Name = "txtOgrenciAra";
+            this.txtOgrenciAra.Size = new System.Drawing.Size(545, 20);
+            this.txtOgrenciAra.TabIndex = 2;
+            this.txtOgrenciAra.Text = "Öğrenci Ara";
+            this.txtOgrenciAra.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtOgrenciAra.MouseEnter += new System.EventHandler(this.txtOgrenciAra_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 461);
+            this.Controls.Add(this.btnResimSec);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtOgrTel);
+            this.Controls.Add(this.txtOgrAdSoyad);
+            this.Controls.Add(this.txtOgrenciAra);
+            this.Controls.Add(this.txtOgrNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOgrNo;
+        private System.Windows.Forms.TextBox txtOgrAdSoyad;
+        private System.Windows.Forms.TextBox txtOgrTel;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnResimSec;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtOgrenciAra;
+    }
+}
+
---
+++ final2Tekrar/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace final2Tekrar
+{
+    public partial class Form1 : Form
+    {
+        OleDbCommand cmd;
+        OleDbConnection con;
+        OleDbDataAdapter da;
+        DataSet ds;
+
+        private string ogrFoto = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ogrenciListele()
+        {
+            con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:/Users/Burak/source/repos/final2Tekrar/final2Tekrar/bin/Debug/vt.mdb");
+            da = new OleDbDataAdapter("SELECT * FROM okul",con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds,"okul");
+            dataGridView1.DataSource = ds.Tables["okul"];
+            con.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ogrenciListele();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ogrNo = txtOgrNo.Text, ogrAdiSoyadi = txtOgrAdSoyad.Text, ogrTel = txtOgrTel.Text;
+            if (ogrNo == "" || ogrAdiSoyadi == "" || ogrTel == "")
+            {
+                MessageBox.Show("Lütfen Verileri Boş Bırakmayınız", "Hata", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                cmd = new OleDbCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "INSERT INTO okul (ogrNo,ogrAdiSoyadi,ogrTel,ogrFoto) VALUES ('"+ogrNo+"','"+ogrAdiSoyadi+"','"+ogrTel+"','"+ogrFoto+"')";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                ogrenciListele();
+            }
+        }
+
+        private void btnResimSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosya = new OpenFileDialog();
+            dosya.Filter = "Resim Dosyası |*.jpg;*.nef;*.png|Tüm Dosyalar |*.*";
+            dosya.ShowDialog();
+            ogrFoto = dosya.FileName;
+            pictureBox1.ImageLocation = ogrFoto;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow.Cells.Count == 0)
+            {
+
+            }
+            else
+            {
+                txtOgrNo.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtOgrAdSoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                txtOgrTel.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                if (dataGridView1.CurrentRow.Cells[4].Value.ToString() == "")
+                {
+                    pictureBox1.ImageLocation = "";
+                }
+                else
+                {
+                    pictureBox1.ImageLocation = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                }
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string ogrNo = txtOgrNo.Text, ogrAdiSoyadi = txtOgrAdSoyad.Text, ogrTel = txtOgrTel.Text;
+            if (ogrNo == "" || ogrAdiSoyadi == "" || ogrTel == "")
+            {
+                MessageBox.Show("Lütfen Verileri Boş Bırakmayınız", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                cmd = new OleDbCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "UPDATE okul SET ogrNo='"+ogrNo+"', ogrAdiSoyadi='"+ogrAdiSoyadi+"', ogrTel='"+ogrTel+"', ogrFoto='"+ogrFoto+"' WHERE ogrNo='" + dataGridView1.CurrentRow.Cells[1].Value.ToString() + "' ";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                ogrenciListele();
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (txtOgrNo.Text == "")
+            {
+                MessageBox.Show("Lütfen Silmek İstenilen Satırı Seçiniz..::..", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string ogrNo = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                cmd = new OleDbCommand();
+                cmd.Connection = con;
+                con.Open();
+                cmd.CommandText = "DELETE FROM okul WHERE ogrNo ='"+ogrNo+"'";
+                cmd.ExecuteNonQuery();
+                con.Close();
+                ogrenciListele();
+            }
+        }
+
+        private void txtOgrenciAra_MouseEnter(object sender, EventArgs e)
+        {
+            txtOgrenciAra.Text = "";
+        }
+    }
+}
